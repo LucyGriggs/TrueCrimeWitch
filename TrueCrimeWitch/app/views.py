c424955616188d9e0b54c22a61680911f4326375
LucyGriggs <70904391+LucyGriggs@users.noreply.github.com>
2024-03-26 21:19:05 +0000

    fixed some things, broke others

--- TrueCrimeWitch/app/views.py
+++ TrueCrimeWitch/app/views.py
@@ -18,18 +18,18 @@ def home(request):
         }
     )
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+# def contact(request):
+#     """Renders the contact page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request,
+#         'app/contact.html',
+#         {
+#             'title':'Contact',
+#             'message':'Your contact page.',
+#             'year':datetime.now().year,
+#         }
+#     )
 
 def about(request):
     """Renders the about page."""
@@ -39,7 +39,36 @@ def about(request):
         'app/about.html',
         {
             'title':'About',
-            'message':'Your application description page.',
+            'message':'About Page/Contact',
             'year':datetime.now().year,
         }
     )
+
+def podcasts(request):
+    """Renders the podcasts page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/podcasts.html',
+        {
+            'title':'Podcasts',
+            'message':'List of Podcast Episodes',
+            'year':datetime.now().year,
+        }
+    )
+    
+# def search(request):
+#     """Renders the search function."""
+#     assert isinstance(request, HttpRequest)
+#     query = request.GET.get('q')
+#     podcast_results = Podcast.objects.filter(title__icontains=query)
+#     blog_results = Index.objects.filter(title__icontains=query)
+#     results = list(podcast_results) + list(blog_results)
+#     return render(request,
+#         'app/search.html',
+#         {
+#             'title':'Search Results',
+#             'results':results,
+#             'year':datetime.now().year,
+#         }
+#     )
