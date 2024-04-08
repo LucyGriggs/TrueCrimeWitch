8375bfa2756a150b0b710a9bafb9d103a843df4e
LucyGriggs <70904391+LucyGriggs@users.noreply.github.com>
2024-04-08 23:31:59 +0100

    posts work
    next to fix images in boxes

--- tcw/settings.py
+++ tcw/settings.py
@@ -109,9 +109,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
+STATIC_URL = '/app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/static')
 
 # default primary keys
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
